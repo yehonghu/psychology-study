b7144e7550397c00ab2d5c5e361e76fadbc36d70
yehonghu <116537239+yehonghu@users.noreply.github.com>
2026-08-18 16:13:32 +0000

    fix: support PsychMind GitHub Pages base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import ScrollProgress from "./components/ScrollProgress";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -11,15 +11,19 @@ import ChaptersIndex from "./pages/ChaptersIndex";
 import QuizPage from "./pages/QuizPage";
 
 function Router() {
+  const base = import.meta.env.BASE_URL === "/" ? "" : import.meta.env.BASE_URL.replace(/\/$/, "");
+
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/chapters" component={ChaptersIndex} />
-      <Route path="/chapter/:id" component={ChapterPage} />
-      <Route path="/quiz" component={QuizPage} />
-      <Route path="/404" component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={base}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/chapters" component={ChaptersIndex} />
+        <Route path="/chapter/:id" component={ChapterPage} />
+        <Route path="/quiz" component={QuizPage} />
+        <Route path="/404" component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
